57670ac4da24771ef2dfa6a63d158c81dc4dd10c
Michele <michelebrunasilva@gmail.com>
2023-10-26 21:06:26 -0300

    Initial commit

---
+++ tests/api/test_user.py
@@ -0,0 +1,10 @@
+# ToDo: 1 - criar um teste que adicione um usuário
+# ToDo: 2 - realizar o login e extrair o token da resposta
+# Todo: 3 - criar uma venda de um pet para o usuário
+# Todo: 4 - consultar os dados do pet que foi vendido
+
+
+def teste_incluir_usuario():
+    # Configura
+    # Dados de Entrada
+
